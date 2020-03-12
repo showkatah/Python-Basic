136a410dada9fadc85d8c75e748aee4ca10fb4fd
SHOWKAT AHMAD <55344100+showkatah@users.noreply.github.com>
2020-03-12 23:11:33 +0530

    Update and rename addmat.py to add-2-matrices.py

--- add-2-matrices.py
+++ add-2-matrices.py
@@ -1,3 +1,7 @@
+"""In this pgm elements of two matrices are first taken in a list using split function.
+Using numpy.reshape() to reshape list elements into a square matrix.
+After that two matrices are added using arithmatic operation '+' """
+
 import numpy as np
 x=int(input('Enter row or column of matrix: '))
 print('Enter',x*x,' elements of matrix A with commas: ')
